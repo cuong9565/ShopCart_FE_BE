c1586f445658c112f4532bbc2fa0fe5e9c9ad185
cuong9565 <cuongmanhle0905@gmail.com>
2026-05-08 15:07:46 +0700

    Fix

--- backend/src/main/java/com/shopcart/service/UserServiceImpl.java
+++ backend/src/main/java/com/shopcart/service/UserServiceImpl.java
@@ -2,13 +2,9 @@ package com.shopcart.service;
 
 import org.springframework.stereotype.Service;
 
-import com.shopcart.entity.User;
-import com.shopcart.repository.UserRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
-    private final UserRepository userRepository;
 }
