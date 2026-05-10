b76f8c96876dd1a1bf1062f5d938247f17be3c5b
tiendat <3123560014@sv.sgu.edu.vn>
2026-05-10 11:55:46 +0700

    Cart Unit Tests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -7,4 +7,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  test: {
+    include: ['src/**/*.test.ts'],
+    exclude: [
+      'node_modules',
+      'dist',
+      'tests',
+      'e2e'
+    ]
+  }
 })
